6d9aff0e1ca82f6cb45e0e05302f265cbab69473
didi <ofisser86@gmail.com>
2021-11-02 14:35:01 +0200

    Create locals

---
+++ 1-locals.tf
@@ -0,0 +1,14 @@
+locals {
+  region               = "us-west2"
+  org_id               = ""
+  billing_account      = ""
+  host_project_name    = "host-staging"
+  service_project_name = "k8s-staging"
+  host_project_id      = "${local.host_project_name}-${random_integer.int.result}"
+  service_project_id   = "${local.service_project_name}-${random_integer.int.result}"
+  projects_api         = "container.googleapis.com"
+  secondary_ip_ranges = {
+    "pod-ip-range"      = "10.0.0.0/14",
+    "services-ip-range" = "10.4.0.0/19"
+  }
+}
